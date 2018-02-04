e67aaf0c0b6c7bb8cebfbe54762fb50295c4bcc1
John Lamis <lamisjohn@gmail.com>
2018-02-04 17:06:41 +0200

    Added the firebase cloud firestore to store the prices and coin data and a web socket to get the prices live, which will soon be replaced by the database

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,61 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+require('firebase/firestore');
 const express = require('express');
+const fs = require('fs');
 const app = express();
+global.fetch = require('node-fetch');
+const cc = require('cryptocompare');
+
+const projectInfo = JSON.parse(fs.readFileSync(__dirname + '/../projectStuff.json'));
+
+firebase.initializeApp({
+    apiKey: projectInfo.apiKey,
+    authDomain: projectInfo.authDomain,
+    projectId: projectInfo.projectId
+});
+
+var db = firebase.firestore();
+
+const coinInfoRef = db.collection('coinInfo');
+
+
+
+function getLatestPrices(){
+
+    coinInfoRef.doc('coins').get().then(doc => {
+
+        if(!doc.exists){
+
+            cc.coinList().then(list => {
+
+                for(coin in list){
+                    // coinInfoRef.doc('coins').set({
+                    //     coin
+                    // }).then(() => {
+                    //     console.log(coin.name, " added!");
+                    // }).catch(error => {
+                    //     console.error("Error writing to database: ", error);
+                    // });
+                    console.log(coin);
+                }
+
+            });
+
+        }
+
+    });
+
+}
+
+
+let refresh = 30; // Seconds after each refresh to get the latest prices
+
+setTimeout(() => {
+    getLatestPrices();
+}, refresh * 1000);
+
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
@@ -9,7 +63,13 @@ const socket = require('socket.io');
 
 
 app.get('/price', (request, response) => {
+
     response.render('price', {name: "John"});
+
+    cc.coinList().then(list => {
+        console.log(list);
+    }).catch(console.error);
+
 });
 
 
